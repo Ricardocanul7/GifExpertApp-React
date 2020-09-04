7e08a55928a374251e85f0e6d88bf69d309a6b6b
Ricardo Canul <ricardocanul7@gmail.com>
2020-09-04 18:23:54 -0500

    Create GifGridItem component to visualize each item from API response.

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,33 +1,46 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { GifGridItem } from './GifGridItem';
 
-const GifGrid = ({category}) => {
+const GifGrid = ({ category }) => {
 
-    const [count, setCount] = useState(0);
+    const [images, setImages] = useState([]);
 
-    useEffect( ()=>{
+    useEffect(() => {
         getGifs();
-    }, [] )
+    }, [])
 
-    const getGifs = async() => {
+    const getGifs = async () => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick and Morty&limit=10&api_key=pur1jAp1bOFh7zzIFHzuSRXSKqyX3Tfr'
         const resp = await fetch(url);
-        const {data} = await resp.json();
+        const { data } = await resp.json();
 
-        const gifs = data.map( img => {
+        const gifs = data.map(img => {
             return {
                 id: img.id,
-                title : img.title,
+                title: img.title,
                 url: img.images.downsized_medium.url
             }
         })
 
         console.log(gifs);
+        setImages(gifs);
     }
 
     return (
         <div>
             <h3>{category}</h3>
+            <ol>
+                {
+                    images.map(image => (
+                        <GifGridItem
+                            key={image.id}
+                            {...image}
+                        />
+                    ))
+                }
+
+            </ol>
         </div>
     )
 }

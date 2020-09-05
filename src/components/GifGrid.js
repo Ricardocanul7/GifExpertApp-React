a6724f6d26549150cb9546b6c4267ae478f9ef4d
Ricardo Canul <ricardocanul7@gmail.com>
2020-09-04 19:33:59 -0500

    Custom hooks and loading validation

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,21 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { GifGridItem } from './GifGridItem';
-import { getGifs } from '../helpers/getGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 const GifGrid = ({ category }) => {
-
-    const [images, setImages] = useState([]);
-
-    useEffect(() => {
-        getGifs(category).then(setImages);
-    }, [category])
-
-    
+    const { data:images,  loading } = useFetchGifs(category);
 
     return (
         <>
             <h3>{category}</h3>
+            { loading && <p>Loading</p> }
             <div className="card-grid">
                 {
                     images.map(image => (

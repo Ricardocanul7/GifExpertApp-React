15054ca707c927cca1fc45b980975bd7f8ae9125
Ricardo Canul <ricardocanul7@gmail.com>
2020-09-10 19:18:22 -0500

    PropTypes added and Snapshot updated

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,8 +3,11 @@ import { shallow } from 'enzyme';
 const { GifGridItem } = require("../../components/GifGridItem")
 
 describe('Pruebas en componentes', () => {
+    const title = 'Un titulo';
+    const url = 'http://localhost/algo.jpg';
+
     test('debe mostrarse GifGridItems correctamente', () => {
-        const wrapper = shallow(<GifGridItem />);
+        const wrapper = shallow(<GifGridItem title={title} url={url}/>);
         expect(wrapper).toMatchSnapshot();
     })
     

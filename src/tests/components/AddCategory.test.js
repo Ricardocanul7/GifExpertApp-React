47c3019af22626fd56ea9d95a8a70a9a311e9372
Ricardo Canul <ricardocanul7@gmail.com>
2020-09-11 01:02:11 -0500

    Test to simulate submit and clean inputValue

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -28,5 +28,22 @@ describe('Pruebas en <AddCategory />', () => {
         expect(setCategories).not.toHaveBeenCalled();
     })
     
+    test('debe de llamar el setCategories y limpiar la caja de texto', () => {
+        // 1.- simular el inputChange
+        const value = 'Hola Mundo';
+        wrapper.find('input').simulate('change', { target: {value : value} });
+
+        // 2.- simular el submit
+        wrapper.find('form').simulate('submit', { preventDefault(){} });
+
+        // 3.- setCategories se debe de haber llamado
+        expect(setCategories).toHaveBeenCalled();
+        expect(setCategories).toHaveBeenCalledTimes(1);
+        expect(setCategories).toHaveBeenCalledWith( expect.any(Function));
+
+        // 4.- el valor del input debe estar ''
+        expect(wrapper.find('input').prop('value')).toBe('');
+    })
+    
     
 })

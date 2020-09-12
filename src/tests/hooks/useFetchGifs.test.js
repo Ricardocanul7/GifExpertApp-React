81d6b933a7255026cf329f93b7b0be220f488306
Ricardo Canul <ricardocanul7@gmail.com>
2020-09-11 22:50:22 -0500

    test customHooks

---
+++ src/tests/hooks/useFetchGifs.test.js
@@ -0,0 +1,13 @@
+import { renderHook } from '@testing-library/react-hooks';
+import {useFetchGifs} from '../../hooks/useFetchGifs';
+
+describe('Pruebas en el hook useFetchGifs', () => {
+    test('debe de retornar el estado inicial', () => {
+        const { result } = renderHook( () => useFetchGifs('One Punch') );
+        const {data, loading } = result.current;
+        
+        expect(data).toEqual([]);
+        expect(loading).toBe(true);
+    })
+    
+})
